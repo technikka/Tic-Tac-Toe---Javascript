c4aca60310904e915427f0cfefc13cdd0ff608ff
Pythonikk <nikkibrusoe@gmail.com>
2022-09-13 12:19:45 -0700

    Change handeling of player name and added events for placing tokens

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const Player = (name, token) => {
+const Player = (token) => {
+
+  let name;
   const getName = () => name;
+  const setName = (input) => { name = input }
   const getToken = () => token;
-  
-  return { getName, getToken }
+
+  return { getName, setName, getToken }
 };
+
 
 const GameBoard = (() => {
   let row1 = Array(3).fill(' ');
@@ -13,49 +17,86 @@ const GameBoard = (() => {
   return { row1, row2, row3 }
 })();
 
+
 const GameControls = (() => {
-  const toggleModal = () => {
+  const toggleSetPlayerModal = () => {
     let modal = document.querySelector('.player-modal');
     let backdrop = document.querySelector('.backdrop');
     modal.classList.toggle('show');
     backdrop.classList.toggle('show');
     document.querySelector('.player-1-name').focus();
   }
+  const displayPlayerTurn = () => {
+    const turnDiv = document.querySelector('.player-turn');
+    turnDiv.textContent = `Turn: ${Game.currentTurn}`
+  }
 
   const setEventListeners = () => {
-    const playBtn = document.querySelector('.play-btn');
-    playBtn.addEventListener('click', event => {
-      toggleModal();
+    const nameBtn = document.querySelector('.name-btn');
+    nameBtn.addEventListener('click', event => {
+      GameControls.toggleSetPlayerModal();
     })
     const setBtn = document.querySelector('.player-modal > section > button');
     setBtn.addEventListener('click', event => {
-      Game.createPlayers();
-      toggleModal();
+      toggleSetPlayerModal();
+      Game.setPlayerNames();
     })
+
+    const playSquares = document.querySelectorAll('.game-board > div > div');
+    for (let i = 0; i < playSquares.length; i++) {
+      playSquares[i].addEventListener('click', event => {
+        Game.placeToken(playSquares[i]);
+      })
+    }
   }
 
-  return { setEventListeners }
+  return { 
+    setEventListeners, 
+    toggleSetPlayerModal,
+    displayPlayerTurn
+  }
 })();
 
+
 const Game = (() => {
-  const _coinFlip = () => {
-    let choices = ['X', 'O'];
-    return choices[Math.round(Math.random())]
-  };
+  var currentTurn = "X";
 
-  const startTurn = _coinFlip();
-
-  var player1 = undefined;
+  var player1 = undefined; 
   var player2 = undefined;
 
   const createPlayers = () => {
-    let name1 = document.querySelector('.player-1-name').value;
-    let name2 = document.querySelector('.player-2-name').value;
-    Game.player1 = Player(name1, "X");
-    Game.player2 = Player(name2, "O");
+    Game.player1 = Player("X");
+    Game.player2 = Player("O");
   }
 
-  return { startTurn, createPlayers, player1, player2 }
+  const setPlayerNames = () => {
+    let name1 = document.querySelector('.player-1-name').value;
+    let name2 = document.querySelector('.player-2-name').value;
+    Game.player1.setName(name1);
+    Game.player2.setName(name2);
+  }
+
+  const placeToken = (square) => {
+    if (currentTurn === "X") {
+      square.textContent = "X";
+      square.style.color = "blue";
+    } else {
+      square.textContent = "O";
+      square.style.color = "green";
+    }
+  }
+
+  return { 
+    createPlayers, 
+    setPlayerNames, 
+    placeToken,
+    currentTurn,  
+    player1, 
+    player2 
+  }
 })();
 
+
 GameControls.setEventListeners();
+Game.createPlayers();
+GameControls.displayPlayerTurn();
