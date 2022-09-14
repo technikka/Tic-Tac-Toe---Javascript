58f9d2dee5d5e32f9d8e9e2967526d0f09abe2ec
Pythonikk <nikkibrusoe@gmail.com>
2022-09-14 13:19:26 -0700

    disallow selecting a square already taken

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const Player = (token) => {
-
   let name;
   const getName = () => name;
   const setName = (input) => { name = input }
@@ -14,7 +13,17 @@ const GameBoard = (() => {
   let row2 = Array(3).fill(' ');
   let row3 = Array(3).fill(' ');
 
-  return { row1, row2, row3 }
+  // row needs 'row1, row2, row3', and column needs 0, 1, 2
+  const updateBoard = (square) => {
+    console.log(GameControls.playableSquare);
+    square.removeEventListener('click', GameControls.playableSquare);
+    square.classList.remove('playable-square');
+
+    // row[column] = Game.getTurn();
+    // console.log(row1, row2, row3);
+  }
+
+  return { updateBoard, row1, row2, row3 }
 })();
 
 
@@ -26,9 +35,23 @@ const GameControls = (() => {
     backdrop.classList.toggle('show');
     document.querySelector('.player-1-name').focus();
   }
+
   const displayPlayerTurn = () => {
     const turnDiv = document.querySelector('.player-turn');
-    turnDiv.textContent = `Turn: ${Game.currentTurn}`
+    const turn = Game.getTurn();
+    if (Game.player1.getName() !== undefined ) {
+      if (turn === 'X') {
+        turnDiv.textContent = `Turn: ${Game.player1.getName()}`
+      } else {
+        turnDiv.textContent = `Turn: ${Game.player2.getName()}`
+      }
+    } else {
+      turnDiv.textContent = `Turn: ${Game.getTurn()}`
+    }
+  }
+
+  const playableSquare = function() {
+    Game.playTurn(this);
   }
 
   const setEventListeners = () => {
@@ -40,29 +63,42 @@ const GameControls = (() => {
     setBtn.addEventListener('click', event => {
       toggleSetPlayerModal();
       Game.setPlayerNames();
+      displayPlayerTurn();
     })
 
     const playSquares = document.querySelectorAll('.game-board > div > div');
+
     for (let i = 0; i < playSquares.length; i++) {
-      playSquares[i].addEventListener('click', event => {
-        Game.placeToken(playSquares[i]);
-      })
+      playSquares[i].addEventListener('click', playableSquare);
+      playSquares[i].classList.add('playable-square');
     }
   }
 
   return { 
     setEventListeners, 
     toggleSetPlayerModal,
-    displayPlayerTurn
+    displayPlayerTurn,
+    playableSquare
   }
 })();
 
 
 const Game = (() => {
   var currentTurn = "X";
-
   var player1 = undefined; 
   var player2 = undefined;
+
+  const getTurn = () => {
+    return currentTurn
+  }
+
+  const toggleTurn = () => {
+    if (currentTurn === "X") {
+      currentTurn = "O"
+    } else {
+      currentTurn = "X"
+    }
+  }
 
   const createPlayers = () => {
     Game.player1 = Player("X");
@@ -86,11 +122,19 @@ const Game = (() => {
     }
   }
 
+  const playTurn = (square) => {
+    placeToken(square);
+    GameBoard.updateBoard(square);
+    toggleTurn();
+    GameControls.displayPlayerTurn();
+  }
+
   return { 
+    playTurn,
     createPlayers, 
     setPlayerNames, 
     placeToken,
-    currentTurn,  
+    getTurn, 
     player1, 
     player2 
   }
